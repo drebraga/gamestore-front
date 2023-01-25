a84c5dfa7e770eca67e4e23330580f1c740f7814
drebraga <dre.braga@gmail.com>
2023-01-24 22:16:46 -0300

    feat: Nova divisão de pastas adicionada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./Styles/Reset"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/login" element={"Login"} />
+        <Route path="/cadastro" element={"Cadastro"} />
+        <Route path="/carrinho" element={"Carrinho"} />
+        {/* <Route path="/:compra?" element={"Pagina Final"} /> */}
+        <Route path="*" element={"Erro, caminho errado"} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
