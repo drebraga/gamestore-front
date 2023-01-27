6b288f6ff11b54cd298a55669e4d01c2c60522b3
drebraga <dre.braga@gmail.com>
2023-01-27 19:30:44 -0300

    feat: jogos são adicionados ao carrinho

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Context from "./Context/Context";
 import GlobalStyle from "./Styles/Reset"
 import SignUp from "./Pages/SignUpPage/SignUp";
 import HomePage from "./Pages/HomePage/HomePage.js";
-import { Cart } from "./pages/Cart";
+import { Cart } from "./Pages/Cart";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState(JSON.parse(localStorage.getItem("token")));
+
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={"Login"} />
-        <Route path="/carrinho" element={<Cart />} />
-        <Route path="/cadastro" element={<SignUp/>} />
-        {/* <Route path="/:compra?" element={"Pagina Final"} /> */}
-        <Route path="*" element={"Erro, caminho errado"} />
-      </Routes>
+      <Context.Provider value={{ token, setToken }} >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={"Login"} />
+          <Route path="/carrinho" element={<Cart />} />
+          <Route path="/cadastro" element={<SignUp />} />
+          {/* <Route path="/:compra?" element={"Pagina Final"} /> */}
+          <Route path="*" element={"Erro, caminho errado"} />
+        </Routes>
+      </Context.Provider>
     </BrowserRouter>
   );
 }
